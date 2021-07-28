87f74bcfdca2487c09886f72a4fcddb914030464
namo-io <namho913.kim@gmail.com>
2021-07-29 01:22:15 +0900

    fix: convert mongodb model

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/spf13/cast v1.3.1
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
+	go.mongodb.org/mongo-driver v1.7.0
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	google.golang.org/grpc v1.39.0
 )
--- pkg/util/util.go
+++ pkg/util/util.go
@@ -4,6 +4,9 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/namo-io/kit/pkg/log"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func Hostname() string {
@@ -25,4 +28,32 @@ func WaitSignal() os.Signal {
 	defer signal.Stop(sigs)
 
 	return <-sigs
+}
+
+func StringToObjectId(str string) (primitive.ObjectID, error) {
+	return primitive.ObjectIDFromHex(str)
+}
+
+func MustStringToObjectId(str string) primitive.ObjectID {
+	objectId, err := primitive.ObjectIDFromHex(str)
+	if err != nil {
+		log.Warnf("must string to objectID is error, msg: %v", err.Error())
+		return primitive.NilObjectID
+	}
+
+	return objectId
+}
+
+func StringsToObjectIds(slice []string) ([]primitive.ObjectID, error) {
+	objectIds := make([]primitive.ObjectID, 0)
+	for _, item := range slice {
+		objectId, err := primitive.ObjectIDFromHex(item)
+		if err != nil {
+			return nil, err
+		}
+
+		objectIds = append(objectIds, objectId)
+	}
+
+	return objectIds, nil
 }
