f545115c43ec17a9a74d313dabe195302f87dd38
namo-io <namho913.kim@gmail.com>
2021-08-22 01:55:12 +0900

    fix: add graphql handlers

--- pkg/http/handlers/handlers.go
+++ pkg/http/handlers/handlers.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-gonic/gin"
 	"github.com/namo-io/kit/pkg/version"
 )
@@ -21,5 +25,25 @@ func Version() gin.HandlerFunc {
 		default:
 			c.String(http.StatusOK, version.Info().String())
 		}
+	}
+}
+
+func Graphql(es graphql.ExecutableSchema) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithCancel(c.Request.Context())
+		s := handler.NewDefaultServer(es)
+		s.Use(handler.ResponseFunc(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+			defer cancel()
+			return next(ctx)
+		}))
+		s.ServeHTTP(c.Writer, c.Request)
+
+		<-ctx.Done()
+	}
+}
+
+func GraphqlPlayground() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		playground.Handler("GraphqlQL PlayGround", c.Request.URL.Path).ServeHTTP(c.Writer, c.Request)
 	}
 }
